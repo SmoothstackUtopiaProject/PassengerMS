e3234ba6c917fb509beaa228ae0f14ae75631c9f
JoshuaTyler-SmoothStack <Joshua.Tyler@smoothstack.com>
2021-04-01 10:58:34 -0400

    Converted back to .sh filetype

---
+++ CloudFormation.sh
@@ -0,0 +1,12 @@
+aws cloudformation deploy
+--stack-name $APPLICATION_NAME
+--template-file ./ECSService.yml
+--parameter-overrides
+  ApplicationName=$APPLICATION_NAME
+  ECRepositoryUri=$AWS_ID/$APPLICATION_REPOSITORY:$COMMIT_HASH
+  DBUsername=$DB_USERNAME
+  DBPassword=$DB_PASSWORD
+  SubnetID=$SUBNET_ID
+  SecurityGroupID=$SECURITY_GROUP_ID
+  TargetGroupARN=$UTOPIA_PASSENGERMS_TARGETGROUP
+--capabilities \"CAPABILITY_IAM\" \"CAPABILITY_NAMED_IAM\"
