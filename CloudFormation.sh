1368f597476b044c94117b4a3e8b2790bbb84ce5
JoshuaTyler-SmoothStack <Joshua.Tyler@smoothstack.com>
2021-04-01 12:02:04 -0400

    Update CloudFormation.sh

--- CloudFormation.sh
+++ CloudFormation.sh
@@ -4,9 +4,12 @@ aws cloudformation deploy \
 --parameter-overrides \
 ApplicationName=$APPLICATION_NAME \
 ECRepositoryUri=$AWS_ID/$APPLICATION_REPOSITORY:$COMMIT_HASH \
+ExecutionRoleARN=${EXECUTION_ROLE_ARN} \
+DBurl=$DB_URL \
 DBUsername=$DB_USERNAME \
 DBPassword=$DB_PASSWORD \
 SubnetID=$SUBNET_ID \
 SecurityGroupID=$SECURITY_GROUP_ID \
 TargetGroupARN=$UTOPIA_PASSENGERMS_TARGETGROUP \
+VpcId=${VPC_ID} \
 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
